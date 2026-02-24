9d944109852ae822259742e3d97c99aea2a8771f
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2026-02-23 19:03:37 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+fruits={"Watermelon","Mango","Blueberry","Rasberry"}
+print(fruits)
+fruits.add("Strawberry")
+print(fruits)
+fruits.pop()
+print(fruits)
+fruits.remove("Watermelon")
+
+
+
+
+Set1 = {"green", "blue"} 
+Set2 = {"blue", "yellow"}
+print(Set1.union(Set2))
+print(Set1.intersection(Set2))
